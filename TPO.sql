5dc72ef1512e13736d916af51c48c761f7913bef
Ian <128194052+IanAgustinOzafran@users.noreply.github.com>
2024-10-29 19:11:01 -0300

    Add files via upload

---
+++ TPO.sql
@@ -0,0 +1,26 @@
+create database cine;
+use cine;
+
+create table sala(
+idSala INT PRIMARY KEY,
+capacidad INT,
+tipoSala VARCHAR(50),
+);
+
+create table funcion(
+idFuncion INT PRIMARY KEY,
+fechaHora DATETIME,
+);
+
+create table pelicula(
+idPelicula INT PRIMARY KEY,
+
+);
+
+create table entrada(
+idEntrada INT PRIMARY KEY,
+);
+
+create table cliente(
+idCliente INT PRIMARY KEY,
+);
